7e24c76a4e9a2f1e2f81d9e800b7e26160ddd357
jhars <jonharlan.io@gmail.com>
2018-06-18 00:31:00 -0400

    Logging User in with Facebook & Email. Using Expo for Build Tool

---
+++ App.js
@@ -0,0 +1,146 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  // ADD YOUR FIREBASE CREDENTIALS
+  apiKey: "AIzaSyDBb1RmMTeCccdQHzScAtBicef1J4J4NJU",
+  authDomain: "react-firebase-741b8.firebaseapp.com",
+  databaseURL: "https://react-firebase-741b8.firebaseio.com",
+  projectId: "react-firebase-741b8",
+  storageBucket: "",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base'
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = ({
+            email: '',
+            password: ''
+        })
+    }
+
+    componentDidMount() {
+      firebase.auth().onAuthStateChanged((user) => {
+        if (user != null) {
+          console.log(user)
+        }
+      })
+    }
+
+    signUpUser = (email, password) => {
+
+        try {
+
+            if (this.state.password.length < 6) {
+                alert("Please enter atleast 6 characters")
+                return;
+            }
+
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+        }
+        catch (error) {
+            console.log(error.toString())
+        }
+    }
+
+    loginUser = (email, password) => {
+
+        try {
+
+            firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+                console.log(user)
+
+            })
+        }
+        catch (error) {
+            console.log(error.toString())
+        }
+    }
+
+    async loginWithFacebook() {
+      const {type,token} = await Expo.Facebook.logInWithReadPermissionsAsync('245880506168484', { permissions: ['public_profile'] })
+
+      if(type == 'success') {
+
+        const credential = firebase.auth.FacebookAuthProvider.credential(token)
+
+        firebase.auth().signInWithCredential(credential).catch((error) => {
+          console.log(error )
+        })
+      }
+    }
+
+    render() {
+        return (
+            <Container style={styles.container}>
+                <Form>
+                    <Item floatingLabel>
+                        <Label>Email</Label>
+                        <Input
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            onChangeText={(email) => this.setState({ email })}
+                        />
+
+                    </Item>
+
+                    <Item floatingLabel>
+                        <Label>Password</Label>
+                        <Input
+                            secureTextEntry={true}
+                            autoCorrect={false}
+                            autoCapitalize="none"
+                            onChangeText={(password) => this.setState({ password })}
+                        />
+                    </Item>
+
+                    <Button style={{ marginTop: 10 }}
+                        full
+                        rounded
+                        success
+                        onPress={() => this.loginUser(this.state.email, this.state.password)}
+                    >
+                        <Text style={{ color: 'white' }}> Login</Text>
+                    </Button>
+
+                    <Button style={{ marginTop: 10 }}
+                        full
+                        rounded
+                        primary
+                        onPress={() => this.signUpUser(this.state.email, this.state.password)}
+                    >
+                        <Text style={{ color: 'white' }}> Sign Up</Text>
+                    </Button>
+
+                    <Button style={{ marginTop: 10 }}
+                        full
+                        rounded
+                        primary
+                        onPress={() => this.loginWithFacebook()}
+                    >
+                        <Text style={{ color: 'white' }}> Login with Facebook</Text>
+                    </Button>
+
+                </Form>
+            </Container>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        padding: 10
+    },
+});
